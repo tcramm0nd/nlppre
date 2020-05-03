f7411f83c234f1a5d7cfaabd8bfd759217fe3d94
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2020-05-03 18:24:16 -0400

    Update nlpre.py
    Added ability to plot ngrams

--- nlpre.py
+++ nlpre.py
@@ -39,19 +39,37 @@ class NLPRE:
         text = re.sub('\w*\d\w*', '', text)
         return text
 ## this doesnt need to be a function! I can apply this to the text object expternally
-    def tokenize(self, text, tokenizer=None):
-        tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
-        return tokenizer.tokenize(text)
-
-    def ngrams(self, n, length):
+    def ngrams(self, n=1, length=10):
         # realizing that ideally you'd want to implement your own versions
         # of these dependencies, to make your function more robust
 
-        vec = CountVectorizer(stop_words=stop_words,
-                                ngram_range=(n,n)).fit(self.text)
-        words = vec.transform(corpus)
+        if 'self.clean_text' not in locals():
+            self.clean()
+
+        vec = CountVectorizer(stop_words='english',
+                                ngram_range=(n,n)).fit(self.clean_text)
+        words = vec.transform(self.clean_text)
         sum_words = words.sum(axis=0)
         word_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
         word_freq =sorted(word_freq, key = lambda x: x[1], reverse=True)
 
         return word_freq[:length]
+
+    def plot_ngram(self, n=1, length=10, color='Blue'):
+        color = color + 's_d'
+        if n == 1:
+            n_gram = 'Unigrams'
+        elif n == 2:
+            n_gram = 'Bigrams'
+        elif n == 3:
+            n_gram = 'Trigrams'
+        else:
+            n_gram = str(n) + '-grams'
+
+        df = pd.DataFrame()
+        df = pd.DataFrame(self.ngrams(n, length), columns=[n_gram, 'Count'])
+        plot = sns.barplot(x='Count', y = n_gram, data=df, palette= color)
+
+        del df
+
+        return plot
